6b31fa1698ac35893911b3aa166a64fa7fa67243
Peter Booker <mail@peterbooker.com>
2013-09-16 15:29:18 +0100

    upgraded version

--- kebo-twitter-feed.php
+++ kebo-twitter-feed.php
@@ -3,7 +3,7 @@
  * Plugin Name: Kebo - Twitter Feed
  * Plugin URI: http://wordpress.org/plugins/kebo-twitter-feed/
  * Description: Connect your site to your Twitter account and display your Twitter Feed on your website effortlessly with a custom widget. 
- * Version: 0.6.6
+ * Version: 0.6.7
  * Author: Kebo
  * Author URI: http://kebopowered.com
  */
@@ -13,7 +13,7 @@ if (!defined('ABSPATH'))
     exit;
 
 if (!defined('KEBO_TWITTER_PLUGIN_VERSION'))
-    define('KEBO_TWITTER_PLUGIN_VERSION', '0.6.6');
+    define('KEBO_TWITTER_PLUGIN_VERSION', '0.6.7');
 if (!defined('KEBO_TWITTER_PLUGIN_URL'))
     define('KEBO_TWITTER_PLUGIN_URL', plugin_dir_url(__FILE__));
 if (!defined('KEBO_TWITTER_PLUGIN_PATH'))
