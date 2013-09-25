27e74e35227b7612e4748fe90781c72cb5ca20b5
Peter Booker <mail@peterbooker.com>
2013-09-25 15:45:06 +0100

    updated error log section, hide when no errors

--- inc/menu.php
+++ inc/menu.php
@@ -10,8 +10,8 @@ function kebo_twitter_menu() {
 
     add_submenu_page(
             'options-general.php', // Parent
-            __('Kebo Twitter', 'kebo_twitter'), // Page Title
-            __('Kebo Twitter', 'kebo_twitter'), // Menu Title
+            __('Kebo Twitter Feed', 'kebo_twitter'), // Page Title
+            __('Kebo Twitter Feed', 'kebo_twitter'), // Menu Title
             'manage_options', // Capability
             'kebo-twitter', // Menu Slug
             'kebo_twitter_menu_render' // Render Function
@@ -30,7 +30,7 @@ function kebo_twitter_menu_render() {
         }
     
     // Collect returned OAuth2 credentials on callback and save in a transient.
-    if (isset($_GET['service']) && isset($_GET['token'])) :
+    if ( isset( $_GET['service'] ) && isset( $_GET['token'] ) ) :
 
             $data = array(
                 'service' => $_GET['service'],
@@ -52,7 +52,7 @@ function kebo_twitter_menu_render() {
             add_settings_error(
                 'kebo-twitter',
                 esc_attr('settings_updated'),
-                __('Connection established with Twitter. You can now display your Twitter Feed on your website using a <a href="' . admin_url('widgets.php') . '">Widget</a>.', 'kebo_twitter'),
+                sprintf( __('Connection established with Twitter. You can now display your Twitter Feed on your website using a <a href="%s">Widget</a>.', 'kebo_twitter'), admin_url('widgets.php') ),
                 'updated'
             );
 
@@ -91,28 +91,22 @@ function kebo_twitter_menu_render() {
             ?>
         </form>
         
+        <?php $errors = get_option( 'kebo_twitter_errors' ); ?>
+        <?php if ( ! empty( $errors ) ) : ?>
+        
         <div class="box">
             
-            <h3>Error Log</h3>
+            <h3><?php _e( 'Twitter API - Error Log', 'kebo_twitter' ); ?></h3>
             
-            <p><?php _e('There is a chance that we may receive errors when making requests to Twitter, which will be displayed below. If errors are occuring consistently please check the key below to see if action is required.', 'kebo_twitter'); ?></p>
-            <?php $errors = get_option( 'kebo_twitter_errors' ); ?>
+            <p><?php _e( 'Any errors received while connecting to Twitter will be displayed below.', 'kebo_twitter' ); ?></p>
 
             <ul class="error-log">
 
                 <li class="header"><span class="date">Date</span> <span class="ref">Code</span> <span class="message">Message</span></li>
 
-                <?php if (isset($errors[0])) : ?>
-
-                    <?php foreach ($errors as $error) : ?>
-                        <li><span class="date"><?php echo date( __("H:i:s - jS M Y"), $error['date'] ); ?></span> <span class="ref"><?php echo $error['code']; ?></span> <span class="message"><?php echo $error['message']; ?></span></li>
-                    <?php endforeach; ?>
-
-                <?php else : ?>
-
-                        <li><?php _e('Good news, there have been no errors received from Twitter recently.', 'kebo_twitter'); ?></li>
-
-                <?php endif; ?>
+                <?php foreach ( $errors as $error ) : ?>
+                    <li><span class="date"><?php echo date_i18n( get_option( 'date_format' ), $error['date'] ); ?></span> <span class="ref"><?php echo $error['code']; ?></span> <span class="message"><?php echo $error['message']; ?></span></li>
+                <?php endforeach; ?>
 
             </ul>
         
@@ -120,18 +114,22 @@ function kebo_twitter_menu_render() {
         
         <div class="box">
             
-            <h3>Common Errors</h3>
+            <h3><?php _e( 'Twitter API - Error Key', 'kebo_twitter' ); ?></h3>
+            
+            <p><?php _e( 'Below is a key of the most common types of error returned by Twitter and a short description.', 'kebo_twitter' ); ?></p>
 
             <?php $options = kebo_get_twitter_options(); ?>
             <?php $requests = ( 60 / $options['kebo_twitter_cache_timer'] ); ?>
 
-            <p><strong>Error 32: Could not authenticate you</strong> - <?php _e("There is a problem with your connection to Twitter, please disconnect and re-connect to the service.", 'kebo_twitter'); ?></p>
-            <p><strong>Error 64: Your account is suspended and is not permitted to access this feature</strong> - <?php _e("Twitter has blocked our access to your account.", 'kebo_twitter'); ?></p>
-            <p><strong>Error 88: Rate limit exceeded</strong> - <?php printf( __("Your account has hit Twitter's limit of 180 requests per hour, we are making %d of those requests. You can edit 'Feed Refresh Rate' in the options above to change this.", 'kebo_twitter'), $requests ); ?></p>
-            <p><strong>Error 89: Invalid or expired token</strong> - <?php _e("There is a problem with your connection to Twitter, please disconnect and re-connect to the service.", 'kebo_twitter'); ?></p>
-            <p><strong>Error 130: Over capacity</strong> - <?php _e("Twitter are currently too busy to reply.", 'kebo_twitter'); ?></p>
+            <p><strong>Error 32: Could not authenticate you</strong> - <?php _e('There is a problem with your connection to Twitter, please disconnect and re-connect to the service.', 'kebo_twitter'); ?></p>
+            <p><strong>Error 64: Your account is suspended and is not permitted to access this feature</strong> - <?php _e('Twitter has blocked our access to your account.', 'kebo_twitter'); ?></p>
+            <p><strong>Error 88: Rate limit exceeded</strong> - <?php printf( __("Your account has hit Twitter's limit of 180 requests per hour, we are making %d of those requests. You can edit the 'Feed Refresh Rate' in the options above to change this.", 'kebo_twitter'), $requests ); ?></p>
+            <p><strong>Error 89: Invalid or expired token</strong> - <?php _e('There is a problem with your connection to Twitter, please disconnect and re-connect to the service.', 'kebo_twitter'); ?></p>
+            <p><strong>Error 130: Over capacity</strong> - <?php _e('Twitter is currently too busy to reply.', 'kebo_twitter'); ?></p>
         
         </div>
+        
+        <?php endif; ?>
         
     </div>
 
