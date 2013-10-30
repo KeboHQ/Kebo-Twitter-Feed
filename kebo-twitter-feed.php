5fe92f81889973799c274464906fd2efa5f4c92c
Peter <Peter@Peter-PC>
2013-10-30 21:42:47 +0000

    dont call the pointer unless we have version 3.3 or up

--- kebo-twitter-feed.php
+++ kebo-twitter-feed.php
@@ -84,62 +84,72 @@ function kebo_twitter_plugin_meta( $links ) {
 }
 add_filter( 'plugin_action_links_kebo-twitter-feed/kebo-twitter-feed.php', 'kebo_twitter_plugin_meta' );
 
-/**
- * Adds a WordPress pointer to Kebo Twitter settings page.
+/*
+ * Check we are on WordPress version 3.3 at least,
+ * before trying to use the WP Pointer.
  */
-function kebo_twitter_pointer_script_style( $hook_suffix ) {
+global $wp_version;
 
-    // Assume pointer shouldn't be shown
-    $enqueue_pointer_script_style = false;
-
-    // Get array list of dismissed pointers for current user and convert it to array
-    $dismissed_pointers = explode( ',', get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
-
-    // Check if our pointer is not among dismissed ones
-    if ( ! in_array( 'kebo_twitter_settings_pointer', $dismissed_pointers ) ) {
-        $enqueue_pointer_script_style = true;
-
-        // Add footer scripts using callback function
-        add_action('admin_print_footer_scripts', 'kebo_twitter_pointer_print_scripts');
-    }
-
-    // Enqueue pointer CSS and JS files, if needed
-    if ( $enqueue_pointer_script_style ) {
-        wp_enqueue_style( 'wp-pointer' );
-        wp_enqueue_script( 'wp-pointer' );
-    }
+if ( version_compare( $wp_version, '3.3', '>=' ) ) {
     
-}
-add_action( 'admin_enqueue_scripts', 'kebo_twitter_pointer_script_style' );
+    /**
+     * Adds a WordPress pointer to Kebo Twitter settings page.
+     */
+    function kebo_twitter_pointer_script_style( $hook_suffix ) {
 
-function kebo_twitter_pointer_print_scripts() {
+        // Assume pointer shouldn't be shown
+        $enqueue_pointer_script_style = false;
 
-    $pointer_content = '<h3>' . __('Connect to your Twitter Account', 'kebo_twitter') . '</h3>';
-    $pointer_content .= '<p>' . __('In just a few clicks we can connect your website to your Twitter account and display your latest Tweets.', 'kebo_twitter') . ' <a href="' . admin_url('options-general.php?page=kebo-twitter') . '">' . __('Get Started Now', 'kebo_twitter') . '</a></p>';
-    ?>
+        // Get array list of dismissed pointers for current user and convert it to array
+        $dismissed_pointers = explode( ',', get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
 
-    <script type="text/javascript">
-        //<![CDATA[
-        jQuery(document).ready(function($) {
-            $('#menu-settings').pointer({
-                content: '<?php echo $pointer_content; ?>',
-                position: {
-                    edge: 'left', // arrow direction
-                    align: 'center' // vertical alignment
-                },
-                pointerWidth: 350,
-                close: function() {
-                    $.post(ajaxurl, {
-                        pointer: 'kebo_twitter_settings_pointer', // pointer ID
-                        action: 'dismiss-wp-pointer'
-                    });
-                }
-            }).pointer('open');
-        });
-        //]]>
-    </script>
+        // Check if our pointer is not among dismissed ones
+        if ( ! in_array( 'kebo_twitter_settings_pointer', $dismissed_pointers ) ) {
+            $enqueue_pointer_script_style = true;
 
-    <?php
+            // Add footer scripts using callback function
+            add_action('admin_print_footer_scripts', 'kebo_twitter_pointer_print_scripts');
+        }
+
+        // Enqueue pointer CSS and JS files, if needed
+        if ( $enqueue_pointer_script_style ) {
+            wp_enqueue_style( 'wp-pointer' );
+            wp_enqueue_script( 'wp-pointer' );
+        }
+
+    }
+    add_action( 'admin_enqueue_scripts', 'kebo_twitter_pointer_script_style' );
+
+    function kebo_twitter_pointer_print_scripts() {
+
+        $pointer_content = '<h3>' . __('Connect to your Twitter Account', 'kebo_twitter') . '</h3>';
+        $pointer_content .= '<p>' . __('In just a few clicks we can connect your website to your Twitter account and display your latest Tweets.', 'kebo_twitter') . ' <a href="' . admin_url('options-general.php?page=kebo-twitter') . '">' . __('Get Started Now', 'kebo_twitter') . '</a></p>';
+        ?>
+
+        <script type="text/javascript">
+            //<![CDATA[
+            jQuery(document).ready(function($) {
+                $('#menu-settings').pointer({
+                    content: '<?php echo $pointer_content; ?>',
+                    position: {
+                        edge: 'left', // arrow direction
+                        align: 'center' // vertical alignment
+                    },
+                    pointerWidth: 350,
+                    close: function() {
+                        $.post(ajaxurl, {
+                            pointer: 'kebo_twitter_settings_pointer', // pointer ID
+                            action: 'dismiss-wp-pointer'
+                        });
+                    }
+                }).pointer('open');
+            });
+            //]]>
+        </script>
+
+        <?php
+    }
+
 }
 
 /*
