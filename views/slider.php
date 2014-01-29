32b2a89c3d648eb91ec4cdf352267cbae7b0b296
Paul Tomblin <ptomblin@xcski.com>
2014-01-28 20:11:40 -0500

    Added a new option to show today's tweets with the absolute time of day instead of relative time.

--- views/slider.php
+++ views/slider.php
@@ -63,10 +63,14 @@ $allowed_html = array(
             <?php
             // Prepare Date Formats
             if ( date( 'Ymd' ) == date( 'Ymd', strtotime( $tweet->created_at ) ) ) {
-                    
-                // Covert created at date into timeago format
-                $created = human_time_diff( date( 'U', strtotime( $tweet->created_at ) ), current_time( 'timestamp', $gmt = 1 ) );
-                    
+
+                if ( $options['kebo_twitter_absolute_timestamp'] == 1 ) {
+                    // Convert created at date into easily readable format.
+                    $created = date_i18n( get_option('time_format'), strtotime( $tweet->created_at ) + $tweet->user->utc_offset );
+                } else {
+                    // Covert created at date into timeago format
+                    $created = human_time_diff( date( 'U', strtotime( $tweet->created_at ) ), current_time( 'timestamp', $gmt = 1 ) );
+                }
             } else {
                 if ($options['kebo_twitter_today_only'] == 1) {
                     continue;
